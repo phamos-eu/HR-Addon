1e89364969700744d8a6af0d303370c5b5f51992
estackhub <spryng.managed@gmail.com>
2022-05-05 01:26:46 +0000

    sub:create weekly working hours doctype

---
+++ hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Jide Olayinka and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Weekly Working Hours', {
+	// refresh: function(frm) {
+
+	// }
+});
