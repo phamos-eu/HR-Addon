aaaf23c702f9c41174bcb84bdb804409bdc5caa0
estackhub <spryng.managed@gmail.com>
2022-05-05 16:41:38 +0000

    Sub, Weekly working hour readable title, total work hours and total hour time set to seconds

--- hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours.js
+++ hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours.js
@@ -6,3 +6,21 @@ frappe.ui.form.on('Weekly Working Hours', {
 
 	// }
 });
+
+frappe.ui.form.on('Daily Hours Detail',{
+	//
+	hours: function(frm, cdt, cdn){
+		//
+		//console.log('Hourly edit');
+		var total_hour = 0;
+		var t_hour_time = 0;
+		
+		$.each(frm.doc.hours|| [], function(i,d){
+			total_hour += flt(d.hours);
+			t_hour_time += flt(d.hours)*60;
+		});
+		frm.set_value("total_work_hours",total_hour);
+		frm.set_value("total_hours_time",t_hour_time);
+		//this.frm.refresh_fields();
+	},
+});
--- hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours.py
+++ hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours.py
@@ -1,8 +1,18 @@
 # Copyright (c) 2022, Jide Olayinka and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.naming import make_autoname
 
 class WeeklyWorkingHours(Document):
-	pass
+	#pass
+	def autoname(self):
+		""""""
+		coy = frappe.db.sql("select abbr from tabCompany where name=%s", self.company)[0][0]
+		e_name = self.employee
+		#name_key = coy+'-.YYYY.-'+e_name
+		name_key = coy+'-.YYYY.-'+e_name+'-.####'
+		self.name = make_autoname(name_key)
+		self.title_hour= self.name
+
