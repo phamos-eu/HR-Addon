cc98522089581395fac00172268fd513a2c6e373
estackhub <spryng.managed@gmail.com>
2022-05-06 01:34:22 +0000

    fix total for weekly working hours, get hours in seconds field and calculate in total working hours in seconds and reject duplicates in week days

--- hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours.js
+++ hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours.js
@@ -2,25 +2,60 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Weekly Working Hours', {
+	setup: function(frm){
+		frm.check_days_for_duplicates = function(frm,row){
+			frm.doc.hours.forEach(tm => {
+				if(!( row.day =='' || tm.idx==row.idx)){
+					if(row.day==tm.day){
+						row.hours = '';	
+						//console.log('Work hour already set for ');				
+					}
+					
+				}
+			});
+		}
+
+		frm.get_total_hours = function(frm){
+			let total_hour = 0;
+			let t_hour_time = 0;
+			frm.doc.hours.forEach(tm=>{
+				total_hour += flt(tm.hours);
+				//in sec total
+				t_hour_time += flt(tm.hour_time);
+			})
+			frm.set_value("total_work_hours",total_hour);
+			// set total in seconds			
+			frm.set_value("total_hours_time",t_hour_time);
+		}
+
+		frm.set_work_hour_in_seconds = function(frm,tm){
+			$.each(frm.doc.hours|| [], function(i,d){
+				d.hour_time = flt(d.hours)*60*60;
+			});
+			
+		}
+	}
+	
 	// refresh: function(frm) {
 
 	// }
 });
 
 frappe.ui.form.on('Daily Hours Detail',{
-	//
+
 	hours: function(frm, cdt, cdn){
-		//
-		//console.log('Hourly edit');
-		var total_hour = 0;
-		var t_hour_time = 0;
+		//set in time		
+		let row = locals[cdt][cdn];
+		frm.set_work_hour_in_seconds(frm);
+
+		frm.check_days_for_duplicates(frm, row);
+		frm.get_total_hours(frm);
 		
-		$.each(frm.doc.hours|| [], function(i,d){
-			total_hour += flt(d.hours);
-			t_hour_time += flt(d.hours)*60;
-		});
-		frm.set_value("total_work_hours",total_hour);
-		frm.set_value("total_hours_time",t_hour_time);
-		//this.frm.refresh_fields();
+		frm.refresh_field('hours');
+		
 	},
+	
+	hours_remove: function(frm,cdt,cdn){
+		frm.get_total_hours(frm);
+	}
 });
