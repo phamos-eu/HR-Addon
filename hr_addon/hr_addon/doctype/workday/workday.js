9d1ebe9e8234d9b322cfd6dac74bd60b9664d471
estackhub <spryng.managed@gmail.com>
2022-05-06 15:01:35 +0000

    created workday doctype with less functionality

---
+++ hr_addon/hr_addon/doctype/workday/workday.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Jide Olayinka and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Workday', {
+	// refresh: function(frm) {
+
+	// }
+});
