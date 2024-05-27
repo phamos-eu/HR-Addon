bf96fc101193c683d430af4dd0914f51b7419dbf
sonali <sonlainarkar121@gmail.com>
2024-05-27 14:20:27 +0200

    feat:update year of weekly working hours of permanant employee

--- hr_addon/hooks.py
+++ hr_addon/hooks.py
@@ -29,7 +29,9 @@ app_license = "MIT"
 
 # include js in page
 # page_js = {"page" : "public/js/file.js"}
-
+doctype_js = {
+   
+}
 # include js in doctype views
 # doctype_js = {"doctype" : "public/js/doctype.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
@@ -124,6 +126,7 @@ app_license = "MIT"
 # 	]
 # }
 
+
 # Testing
 # -------
 
@@ -196,8 +199,13 @@ doc_events = {
     }
 }
 
+doctype_list_js = {"Weekly Working Hours" : "public/js/list_view.js"}
+
 scheduler_events = {
 	"hourly": [
 		"hr_addon.hr_addon.doctype.hr_addon_settings.hr_addon_settings.generate_workdays_scheduled_job"
 	],
+    "yearly": [
+        "hr_addon.custom_scripts.custom_python.weekly_working_hours.set_from_to_dates",
+	]
 }
