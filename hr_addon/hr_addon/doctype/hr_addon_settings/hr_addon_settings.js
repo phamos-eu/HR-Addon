5d26d3f52f0d5a3391b146ad42e2ce72f296f240
beingeek <furqan.790@outlook.com>
2024-06-05 11:53:42 +0000

    fix: undo testing code

--- hr_addon/hr_addon/doctype/hr_addon_settings/hr_addon_settings.js
+++ hr_addon/hr_addon/doctype/hr_addon_settings/hr_addon_settings.js
@@ -3,35 +3,20 @@
 
 frappe.ui.form.on('HR Addon Settings', {
 	refresh: function(frm) {
-        frm.add_custom_button(
-            __("Test"),
-            function () {
-                frappe.call({
-                    method: "hr_addon.hr_addon.api.utils.send_work_anniversary_notification",
-                    args: {
-                        doc: frm.doc,
-                    },
-                    freeze: true,
-                    callback: function (r) {
-                        
-                    },
-                });
-            },
-        );
 	},
 
-	// validate: function(frm){
-	// 	if (frm.doc.name_of_calendar_export_ics_file.length == 0){
-	// 		const randomString = generateRandomString(24);
-	// 		frm.set_value("name_of_calendar_export_ics_file", randomString)
-	// 	}
-	// },
+	validate: function(frm){
+		if (frm.doc.name_of_calendar_export_ics_file.length == 0){
+			const randomString = generateRandomString(24);
+			frm.set_value("name_of_calendar_export_ics_file", randomString)
+		}
+	},
 
-	// after_save: function(frm){
-	// 	if (frm.doc.name_of_calendar_export_ics_file.length < 24){
-	// 		frappe.msgprint("The filename is less than 24 characters. Please, consider to have a longer filename or leave it empty to get a random filename.")
-	// 	}
-	// },
+	after_save: function(frm){
+		if (frm.doc.name_of_calendar_export_ics_file.length < 24){
+			frappe.msgprint("The filename is less than 24 characters. Please, consider to have a longer filename or leave it empty to get a random filename.")
+		}
+	},
 
 	download_ics_file: function(frm){
 		frappe.call({
