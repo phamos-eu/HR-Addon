f6ae9ebb8b9fa0b36f2e65b28d5beabe177f4a5d
roquegv <roquegv@gmail.com>
2024-06-11 10:39:17 -0400

    fix errors in the HR Addon Setting

--- hr_addon/hr_addon/doctype/hr_addon_settings/hr_addon_settings.js
+++ hr_addon/hr_addon/doctype/hr_addon_settings/hr_addon_settings.js
@@ -6,14 +6,14 @@ frappe.ui.form.on('HR Addon Settings', {
 	},
 
 	validate: function(frm){
-		if (frm.doc.name_of_calendar_export_ics_file.length == 0){
+		if (frm.doc.name_of_calendar_export_ics_file && frm.doc.name_of_calendar_export_ics_file.length == 0){
 			const randomString = generateRandomString(24);
 			frm.set_value("name_of_calendar_export_ics_file", randomString)
 		}
 	},
 
 	after_save: function(frm){
-		if (frm.doc.name_of_calendar_export_ics_file.length < 24){
+		if (frm.doc.name_of_calendar_export_ics_file && frm.doc.name_of_calendar_export_ics_file.length < 24){
 			frappe.msgprint("The filename is less than 24 characters. Please, consider to have a longer filename or leave it empty to get a random filename.")
 		}
 	},
--- hr_addon/hr_addon/doctype/hr_addon_settings/hr_addon_settings.py
+++ hr_addon/hr_addon/doctype/hr_addon_settings/hr_addon_settings.py
@@ -10,9 +10,10 @@ class HRAddonSettings(Document):
 	def before_save(self):
 		# remove the old ics file
 		old_doc = self.get_doc_before_save()
-		old_file_name = old_doc.name_of_calendar_export_ics_file
-		if old_file_name != self.name_of_calendar_export_ics_file:
-			os.remove("{}/public/files/{}.ics".format(frappe.utils.get_site_path(), old_file_name))
+		if old_doc:
+			old_file_name = old_doc.name_of_calendar_export_ics_file
+			if old_file_name != self.name_of_calendar_export_ics_file:
+				os.remove("{}/public/files/{}.ics".format(frappe.utils.get_site_path(), old_file_name))
 
 		# remove also the Urlaubskalender.ics, if exist
 		if os.path.exists("{}/public/files/Urlaubskalender.ics".format(frappe.utils.get_site_path())):
