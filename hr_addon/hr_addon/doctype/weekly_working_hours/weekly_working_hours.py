c9ba1844feb1e76cf6c07150a046e4436fbe9904
sonali <sonlainarkar121@gmail.com>
2024-07-03 11:16:25 +0200

    feat:validation added to prevent duplication of weekly working hours

--- hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours.py
+++ hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours.py
@@ -4,6 +4,7 @@
 import frappe
 from frappe.model.document import Document
 from frappe.model.naming import make_autoname
+from frappe import _, bold
 
 class WeeklyWorkingHours(Document):
 	#pass
@@ -15,4 +16,23 @@ class WeeklyWorkingHours(Document):
 		name_key = coy+'-.YYYY.-'+e_name+'-.####'
 		self.name = make_autoname(name_key)
 		self.title_hour= self.name
+
+	def validate(self):
+		self.validate_record()
+
+	def validate_record(self):
+		existing_record =  frappe.db.get_value("Weekly Working Hours",
+		{
+			"employee": self.employee,
+			"valid_from": self.valid_from,
+			"valid_to":self.valid_to,
+			"name": ("!=", self.name),
+			"docstatus":1
+		}, "name")
+		if existing_record :
+			frappe.throw(
+					_("Weekly Working Hours, already exists for this Employee {0}. Existing record : {1}").format(self.employee,existing_record)
+				)
+		
+
 
