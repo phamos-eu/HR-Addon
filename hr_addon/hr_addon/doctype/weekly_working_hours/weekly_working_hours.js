1cff8fbf2ee37a7e5a087329b5bf55c9be22cbe3
beingeek <furqan.790@outlook.com>
2025-03-26 06:02:30 +0000

    fix: update copyright information and author details to reflect phamos as publisher

--- hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours.js
+++ hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2022, Jide Olayinka and contributors
+// Copyright (c) 2022, phamos.eu and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('Weekly Working Hours', {
