90cfe4425b8fdfa62df2c70b94b42855941de89b
beingeek <furqan.790@outlook.com>
2025-03-27 12:50:57 +0000

    feat: add status field to workday and employee log for improved attendance tracking

--- hr_addon/hr_addon/doctype/workday/workday.py
+++ hr_addon/hr_addon/doctype/workday/workday.py
@@ -32,6 +32,7 @@ class Workday(Document):
 		self.first_checkin = new_workday_dict.get("first_checkin")
 		self.last_checkout = new_workday_dict.get("last_checkout")
 		self.attendance = new_workday_dict.get("attendance")
+		self.status = new_workday_dict.get("status")
 
 		employee_checkins = new_workday_dict.get("employee_checkins") or []
 
@@ -297,6 +298,7 @@ def get_actual_employee_log(aemployee, adate):
                 "hours_worked": 0,
                 "nbreak": 0,
                 "attendance": view_employee_attendance[0].name if len(view_employee_attendance) > 0 else "",
+				"status": view_employee_attendance[0].status if len(view_employee_attendance) > 0 else "",
                 "break_hours": 0,
                 "employee_checkins": [],
                 "first_checkin": "",
@@ -312,6 +314,7 @@ def get_actual_employee_log(aemployee, adate):
                 "hours_worked": 0,
                 "nbreak": 0,
                 "attendance": view_employee_attendance[0].name if len(view_employee_attendance) > 0 else "",
+				"status": view_employee_attendance[0].status if len(view_employee_attendance) > 0 else "",
                 "break_hours": 0,
                 "employee_checkins": [],
                 "first_checkin": "",
@@ -367,6 +370,7 @@ def get_workday(employee_checkins, employee_default_work_hour, no_break_hours, i
     hours_worked = flt(hours_worked)
     actual_working_hours = hours_worked - expected_break_hours
     attendance = employee_checkins[0].attendance if len(employee_checkins) > 0 else ""
+    status = frappe.db.get_value("Attendance", attendance, "status") if attendance else ""
 
     if no_break_hours and hours_worked < 6: # TODO: set 6 as constant
         break_minutes = 0
@@ -392,7 +396,8 @@ def get_workday(employee_checkins, employee_default_work_hour, no_break_hours, i
         "expected_break_hours": expected_break_hours,
         "actual_working_hours": actual_working_hours,
         "nbreak": 0,
-        "attendance": attendance,        
+        "attendance": attendance,
+		"status": status,
         "break_hours": break_hours,
         "first_checkin": first_checkin,
         "last_checkout": last_checkout,
@@ -409,7 +414,7 @@ def get_employee_attendance(employee,atime):
         .select(
             Attendance.name,
             Attendance.employee,
-            Attendance.status,
+            # Attendance.status,
             Attendance.attendance_date,
             Attendance.shift
         )
