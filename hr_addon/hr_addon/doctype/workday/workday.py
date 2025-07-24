3a8074e53a865c693d183cfb5ee189744f8ac16f
beingeek <furqan.790@outlook.com>
2025-07-24 10:07:15 +0000

    Add workday break calculation mechanism and swap hours option in HR Addon settings and functionality in workday

--- hr_addon/hr_addon/doctype/workday/workday.py
+++ hr_addon/hr_addon/doctype/workday/workday.py
@@ -326,15 +326,18 @@ def get_actual_employee_log(aemployee, adate):
 
 
 def get_workday(employee_checkins, employee_default_work_hour, no_break_hours):
+    hr_addon_settings = frappe.get_cached_doc("HR Addon Settings")
+    is_break_from_checkins_with_swapped_hours = hr_addon_settings.workday_break_calculation_mechanism == "Break Hours from Employee Checkins" and hr_addon_settings.swap_hours_worked_and_actual_working_hours
     new_workday = {}
 
     hours_worked = 0.0
-    break_hours = 0.0
+    total_duration = 0
     first_checkin = ""
     last_checkout = ""
 
     # not pair of IN/OUT either missing
     if len(employee_checkins)% 2 != 0:
+        hours_worked = -36.0
         employee_checkin_message = ""
         for d in employee_checkins:
             employee_checkin_message += "<li>CheckIn Type:{0} for {1}</li>".format(d.log_type, frappe.get_desk_link("Employee Checkin", d.name))
@@ -351,43 +354,70 @@ def get_workday(employee_checkins, employee_default_work_hour, no_break_hours):
         for i in range(len(clockin_list)):
             wh = time_diff_in_hours(clockout_list[i],clockin_list[i])
             hours_worked += float(str(wh))
-        
-        # get total break hours
-        for i in range(len(clockout_list)):
-            if ((i+1) < len(clockout_list)):
-                wh = time_diff_in_hours(clockin_list[i+1],clockout_list[i])
-                break_hours += float(str(wh))
 
+        # Calculate difference between first check-in and last checkout
         if clockin_list and clockout_list:
             first_checkin = clockin_list[0]
-            last_checkout = clockout_list[-1] 
+            last_checkout = clockout_list[-1]  # Last element of clockout_list
+            total_duration = time_diff_in_hours(last_checkout, first_checkin) 
 
-    break_minutes = employee_default_work_hour.break_minutes
+        if is_break_from_checkins_with_swapped_hours:
+            total_duration, hours_worked = hours_worked, total_duration
+
+    default_break_minutes = employee_default_work_hour.break_minutes
+    default_break_hours = flt(default_break_minutes / 60)
     target_hours = employee_default_work_hour.hours
 
-    expected_break_hours = flt(break_minutes / 60)
-    break_hours = flt(break_hours)
+    if len(employee_checkins) % 2 == 0:
+        break_from_checkins = 0.0
+        for i in range(len(clockout_list) - 1):
+            wh = time_diff_in_hours(clockin_list[i + 1], clockout_list[i])
+            break_from_checkins += float(wh)
+
+        if hr_addon_settings.workday_break_calculation_mechanism == "Break Hours from Employee Checkins":
+            break_hours = break_from_checkins
+
+        elif hr_addon_settings.workday_break_calculation_mechanism == "Break Hours from Weekly Working Hours":
+            break_hours = default_break_hours
+
+        elif hr_addon_settings.workday_break_calculation_mechanism == "Break Hours from Weekly Working Hours if Shorter breaks":
+            if break_from_checkins <= default_break_hours:
+                break_hours = default_break_hours
+            else:
+                break_hours = break_from_checkins
+        else:
+            break_hours = 0.0
+
+    else:
+        break_hours = flt(-360.0)
+
     hours_worked = flt(hours_worked)
-    actual_working_hours = hours_worked - expected_break_hours
+
+    if is_break_from_checkins_with_swapped_hours:
+        #swapping for gall
+        if hours_worked > 0:
+            actual_working_hours = hours_worked - break_hours
+        else:
+            actual_working_hours = total_duration - default_break_hours
+
+    else:
+        if total_duration > 0:
+            actual_working_hours = total_duration - break_hours
+        else:    
+            actual_working_hours = hours_worked - default_break_hours
     attendance = employee_checkins[0].attendance if len(employee_checkins) > 0 else ""
     status = frappe.db.get_value("Attendance", attendance, "status") if attendance else ""
 
-    if no_break_hours and hours_worked < 6: # TODO: set 6 as constant
-        break_minutes = 0
-        expected_break_hours = 0
+    if no_break_hours and hours_worked < 6 and not is_break_from_checkins_with_swapped_hours: # TODO: set 6 as constant
+        default_break_minutes = 0
+        #expected_break_hours = 0
         actual_working_hours = hours_worked
-
-    hr_addon_settings = frappe.get_doc("HR Addon Settings")
-    if hr_addon_settings.enable_default_break_hour_for_shorter_breaks:
-        default_break_hours = flt(employee_default_work_hour.break_minutes/60)
-        if break_hours <= default_break_hours:
-            break_hours = flt(default_break_hours)
 
     new_workday.update({
         "target_hours": target_hours,
-        "break_minutes": break_minutes,
+        "break_minutes": default_break_minutes,
         "hours_worked": hours_worked,
-        "expected_break_hours": expected_break_hours,
+        "expected_break_hours": default_break_hours,
         "actual_working_hours": actual_working_hours,
         "nbreak": 0,
         "attendance": attendance,
