7a282568fcc577d3f2503ce9fab71cfc4eb4e735
sonali <sonali.narkar@phamos.eu>
2026-01-16 11:46:20 +0100

    show workday summery after end of all server messages

--- hr_addon/hr_addon/doctype/workday/workday_list.js
+++ hr_addon/hr_addon/doctype/workday/workday_list.js
@@ -284,13 +284,16 @@ frappe.listview_settings['Workday'] = {
 																	const finalMessage = summaryMessage.length
 																		? summaryMessage
 																		: __("No workdays were created.");
-																	console.log(finalMessage);
-																	frappe.msgprint(
-																		{
-																			title: __('Workdays Summary'),
-																			message: finalMessage,
-																		}
-																	);
+																	const showSummary = () => {
+																		console.log(finalMessage);
+																		frappe.msgprint(
+																			{
+																				title: __('Workdays Summary'),
+																				message: finalMessage,
+																			}
+																		);
+																	};
+																	frappe.after_server_call().then(showSummary);
 																		
 																	frappe.show_alert({
 																		message: __("Workdays Processed"),
