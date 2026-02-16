6684ba662bf1e3aad74eab2fe3110b76db1b0569
sonali <sonali.narkar@phamos.eu>
2026-02-16 11:06:27 +0100

    update calculation for actual working hours

--- hr_addon/hr_addon/doctype/workday/workday.py
+++ hr_addon/hr_addon/doctype/workday/workday.py
@@ -587,7 +587,15 @@ def get_workday(employee_checkins, employee_default_work_hour, no_break_hours):
 
     hours_worked = flt(hours_worked)
 
-    if is_break_from_checkins_with_swapped_hours:
+    if hr_addon_settings.workday_break_calculation_mechanism == "Break Hours from Weekly Working Hours if Shorter breaks":
+        # For this mechanism: use total_duration (total presence on site) minus break_hours
+        # total_duration is time from first checkin to last checkout (includes breaks)
+        if total_duration > 0:
+            actual_working_hours = total_duration - break_hours
+        else:
+            actual_working_hours = total_duration - default_break_hours
+
+    elif is_break_from_checkins_with_swapped_hours:
         # When swap is enabled: hours_worked contains total_duration (after swap at line 517)
         # and total_duration contains sum of work periods, so use total_duration - break_hours
         if hours_worked > 0:
