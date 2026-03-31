879e4b52c1ec3e639331ea56921f35d493ed519f
Robera16 <roberaworknehrobwork@gmail.com>
2026-03-31 17:26:17 +0300

    removed requirements.txt file

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
-
-# get version from __version__ variable in hr_addon/__init__.py
-from hr_addon import __version__ as version
-
 setup(
 	name="hr_addon",
-	version=version,
+	version="0.1.0",
 	description="Addon for Erpnext attendance and employee checkins",
-	author="phamos.eu",
+	author="Phamos GmbH",
 	author_email="support@phamos.eu",
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires
+	install_requires=["icalendar"],
 )
