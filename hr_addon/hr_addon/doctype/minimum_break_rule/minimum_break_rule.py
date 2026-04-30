25681bd4d42444d4a60e4da0db56d4ced131176c
sonali <sonali.narkar@phamos.eu>
2026-04-30 13:06:01 +0200

    added child table

---
+++ hr_addon/hr_addon/doctype/minimum_break_rule/minimum_break_rule.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, phamos.eu and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class MinimumBreakRule(Document):
+	pass
