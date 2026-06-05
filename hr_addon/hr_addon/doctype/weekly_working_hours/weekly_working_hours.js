ba944b77d66dca097fd0d6f0499a8a42d2cc32fa
sonali <sonali.narkar@phamos.eu>
2026-06-05 11:24:50 +0200

    added unit test, removed validation, hide break time

--- hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours.js
+++ hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours.js
@@ -1,45 +1,154 @@
 // Copyright (c) 2022, phamos.eu and contributors
 // For license information, please see license.txt
 
+const MECHANISM_MINIMUM_BREAK_RULE = 'Break Hours from Minimum Break Rule';
+const BREAK_MINUTES_HIDE_STYLE_ID = 'wwh-hide-break-minutes-style';
+
+function ensure_break_minutes_hide_style() {
+	if (document.getElementById(BREAK_MINUTES_HIDE_STYLE_ID)) {
+		return;
+	}
+
+	const style = document.createElement('style');
+	style.id = BREAK_MINUTES_HIDE_STYLE_ID;
+	style.textContent = `
+		.weekly-working-hours-hide-break-minutes .grid-heading-row [data-fieldname="break_minutes"],
+		.weekly-working-hours-hide-break-minutes .grid-row [data-fieldname="break_minutes"],
+		.weekly-working-hours-hide-break-minutes .grid-row-open [data-fieldname="break_minutes"] {
+			display: none !important;
+		}
+	`;
+	document.head.appendChild(style);
+}
+
+function update_break_minutes_docfield(frm, hidden) {
+	// Child table column: fieldname, property, value, parent_docname, child_fieldname
+	frm.set_df_property('hours', 'hidden', hidden, frm.docname, 'break_minutes');
+
+	const child_df = frappe.meta.get_docfield(
+		'Daily Hours Detail',
+		'break_minutes',
+		frm.docname
+	);
+	if (child_df) {
+		child_df.hidden = hidden;
+	}
+}
+
+function refresh_hours_grid_columns(frm, hide) {
+	const hidden = hide ? 1 : 0;
+	const grid = frm.fields_dict.hours?.grid;
+	if (!grid) {
+		return false;
+	}
+
+	update_break_minutes_docfield(frm, hidden);
+
+	const df = grid.get_docfield('break_minutes');
+	if (df) {
+		df.hidden = hidden;
+	}
+
+	grid.docfields?.forEach((docfield) => {
+		if (docfield.fieldname === 'break_minutes') {
+			docfield.hidden = hidden;
+		}
+	});
+
+	if (grid.user_defined_columns?.length) {
+		grid.user_defined_columns = grid.user_defined_columns.filter(
+			(column) => column.fieldname !== 'break_minutes' || !hide
+		);
+	}
+
+	ensure_break_minutes_hide_style();
+	frm.fields_dict.hours.$wrapper
+		.closest('.frappe-control')
+		.toggleClass('weekly-working-hours-hide-break-minutes', hide);
+
+	// Frappe caches visible_columns; clearing it is required to rebuild the header row.
+	grid.visible_columns = [];
+	grid.refresh();
+	return true;
+}
+
+function set_break_minutes_column_visibility(frm, hide) {
+	frm.set_df_property('no_break_hours', 'read_only', hide ? 1 : 0);
+	update_break_minutes_docfield(frm, hide ? 1 : 0);
+
+	const apply_to_grid = () => refresh_hours_grid_columns(frm, hide);
+
+	if (!apply_to_grid()) {
+		let attempts = 0;
+		const interval = setInterval(() => {
+			attempts += 1;
+			if (apply_to_grid() || attempts >= 30) {
+				clearInterval(interval);
+			}
+		}, 100);
+	}
+}
+
+function apply_hr_addon_weekly_form_settings(frm) {
+	frappe.db
+		.get_value(
+			'HR Addon Settings',
+			'HR Addon Settings',
+			'workday_break_calculation_mechanism'
+		)
+		.then((r) => {
+			const hide =
+				r.message?.workday_break_calculation_mechanism ===
+				MECHANISM_MINIMUM_BREAK_RULE;
+			set_break_minutes_column_visibility(frm, hide);
+		});
+}
+
 frappe.ui.form.on('Weekly Working Hours', {
-	setup: function(frm){
-		frm.check_days_for_duplicates = function(frm,row){
-			frm.doc.hours.forEach(tm => {
-				if(!( row.day =='' || tm.idx==row.idx)){
-					if(row.day==tm.day){
-						row.hours = '';	
+	setup(frm) {
+		frm.check_days_for_duplicates = function (frm, row) {
+			frm.doc.hours.forEach((tm) => {
+				if (!(row.day == '' || tm.idx == row.idx)) {
+					if (row.day == tm.day) {
+						row.hours = '';
 					}
-					
 				}
 			});
-		}
+		};
 
-		frm.get_total_hours = function(frm){
+		frm.get_total_hours = function (frm) {
 			let total_hour = 0;
-			frm.doc.hours.forEach(tm=>{
+			frm.doc.hours.forEach((tm) => {
 				total_hour += flt(tm.hours);
-			})
-			frm.set_value("total_work_hours",total_hour);
-		}
-
-		frm.set_work_hour_in_seconds = function(frm,tm){
-			$.each(frm.doc.hours|| [], function(i,d){
-				d.hour_time = flt(d.hours)*60*60;
 			});
-			
-		}
-	}
+			frm.set_value('total_work_hours', total_hour);
+		};
+
+		frm.set_work_hour_in_seconds = function (frm) {
+			$.each(frm.doc.hours || [], function (i, d) {
+				d.hour_time = flt(d.hours) * 60 * 60;
+			});
+		};
+	},
+	onload(frm) {
+		apply_hr_addon_weekly_form_settings(frm);
+	},
+	refresh(frm) {
+		apply_hr_addon_weekly_form_settings(frm);
+	},
 });
 
-frappe.ui.form.on('Daily Hours Detail',{
-	hours: function(frm, cdt, cdn){
+frappe.ui.form.on('Daily Hours Detail', {
+	hours(frm, cdt, cdn) {
 		let row = locals[cdt][cdn];
 		frm.set_work_hour_in_seconds(frm);
 		frm.check_days_for_duplicates(frm, row);
 		frm.get_total_hours(frm);
-		frm.refresh_field('hours');
 	},
-	hours_remove: function(frm,cdt,cdn){
+	hours_remove(frm) {
 		frm.get_total_hours(frm);
-	}
+	},
+	form_render(frm) {
+		apply_hr_addon_weekly_form_settings(frm);
+	},
 });
--- hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours.py
+++ hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours.py
@@ -3,12 +3,9 @@
 
 import frappe
 from frappe.model.document import Document
-from frappe.utils import getdate, flt, cint
+from frappe.utils import getdate, flt
 from frappe.model.naming import make_autoname
 from frappe import _
-from math import inf
-
-MECHANISM_MINIMUM_BREAK_RULE = "Break Hours from Minimum Break Rule"
 
 
 class WeeklyWorkingHours(Document):
@@ -29,69 +26,6 @@ class WeeklyWorkingHours(Document):
 	def validate(self):
 		self.validate_if_employee_is_active()
 		self.validate_overlapping_records_in_specific_interval()
-		self.set_break_hours_if_not_set()
-
-	def _get_rule_break_minutes(self, rules, target_hours):
-		for rule_index, rule in enumerate(rules):
-			from_hours = flt(rule.from_hours or 0)
-			to_hours = flt(rule.to_hours) if rule.to_hours is not None else None
-
-			# Boundary semantics requested:
-			# - first row: from_hours <= target_hours <= to_hours
-			# - next rows: from_hours < target_hours <= to_hours
-			# - open-ended row: target_hours > from_hours (except first row, which is >=)
-			in_lower_bound = target_hours >= from_hours if rule_index == 0 else target_hours > from_hours
-			in_upper_bound = True if to_hours is None else target_hours <= to_hours
-			if in_lower_bound and in_upper_bound:
-				return cint(rule.minimum_break_minutes or 0)
-
-		return None
-
-	def set_break_hours_if_not_set(self):
-		if not self.hours:
-			return
-
-		settings = frappe.get_single("HR Addon Settings")
-		if settings.workday_break_calculation_mechanism != MECHANISM_MINIMUM_BREAK_RULE:
-			return
-
-		rules = sorted(
-			(settings.minimum_break_rule or []),
-			key=lambda row: (
-				flt(row.from_hours or 0),
-				flt(row.to_hours) if row.to_hours is not None else inf,
-			),
-		)
-		if not rules:
-			return
-
-		for hour_row in self.hours:
-			target_hours = flt(hour_row.hours)
-			current_break_minutes = cint(hour_row.break_minutes or 0)
-			if target_hours <= 0:
-				continue
-
-			required_break_minutes = self._get_rule_break_minutes(rules, target_hours)
-			if required_break_minutes is None:
-				continue
-
-			if current_break_minutes == 0:
-				hour_row.break_minutes = required_break_minutes
-				continue
-
-			if current_break_minutes < required_break_minutes:
-				# If user enters lower break than the mandatory rule, reset it
-				# back to the matched minimum instead of blocking save.
-				row_label = hour_row.day or _("Unknown day")
-				frappe.msgprint(
-					_(
-						"Break Minutes for row '{0}' cannot be less than {1} "
-						"for target hours {2}."
-					).format(row_label, required_break_minutes, target_hours),
-					alert=True,
-					indicator="orange",
-				)
-				hour_row.break_minutes = required_break_minutes
 
 	def validate_if_employee_is_active(self):
 		if self.employee and frappe.get_value('Employee', self.employee, 'status') != "Active":
