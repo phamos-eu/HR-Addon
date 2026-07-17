bfe15d9dc5476c72c8a12f77a91a729a4736c783
sonali <sonali.narkar@phamos.eu>
2026-07-17 23:33:00 +0200

    add naming series field

---
+++ hr_addon/hr_addon/doctype/workday_generation_log/workday_generation_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, phamos.eu and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Workday Generation Log", {
+// 	refresh(frm) {
+
+// 	},
+// });
